4c18bd834c80b0dacd6daf6f3759a2d1195c8c4f
thedoomx <stani_b_sf@abv.bg>
2016-03-31 16:13:08 +0300

    commit week12day02

---
+++ Week12Day02MVVM/EmployeesWindow/DataAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesWindow
+{
+    static class DataAccess
+    {
+        static NorthwindEntities context = new NorthwindEntities();
+
+        public static NorthwindEntities Context { get { return context; } }
+
+        public static void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+        public static List<Product> GetProductsForOrder(int orderId)
+        {
+            var order = context.Orders.Where(x => x.OrderID == orderId).FirstOrDefault();
+            if (order == null)
+            {
+                return new List<Product>();
+            }
+
+            return order.Order_Details.Select(x => x.Product).ToList();
+        }
+    }
+}
---
+++ Week12Day02MVVM/EmployeesWindow/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmployeesWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel viewModel = new MainWindowViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+        private void employeesGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            viewModel.SelectedEmployee = employeesGrid.SelectedItem as Employee;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.SaveChanges();
+        }
+    }
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private Employee selectedEmployee;
+
+        public List<Employee> Employees
+        {
+            get
+            {
+                return DataAccess.Context.Employees.ToList();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Employee SelectedEmployee
+        {
+            get
+            {
+                return selectedEmployee;
+            }
+            set
+            {
+                selectedEmployee = value;
+                OnPropertyChanged("SelectedEmployee");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
